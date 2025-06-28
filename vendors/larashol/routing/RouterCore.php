608c9b4184887a22c0e57af3f4076aeaaf32ea07
iman hashemi <hashemi.iman@gmail.com>
2025-06-28 18:47:06 +0330

    router-core

--- vendors/larashol/routing/RouterCore.php
+++ vendors/larashol/routing/RouterCore.php
@@ -12,7 +12,7 @@ class RouterCore {
         'uri' => $uri,
         'method' => 'GET',
         'controller' => $params[0],
-        'method' => $params[1],
+        'function' => $params[1],
      ];
    }
 
@@ -22,13 +22,36 @@ class RouterCore {
         'uri' => $uri,
         'method' => 'POST',
         'controller' => $params[0],
-        'method' => $params[1],
+        'function' => $params[1],
      ];
    }
 
-   public function resolve(){
+   public function resolve($url,$method){
 
-       dd($this);
+      $id=null;
+      $uri = explode('/',$url);
+      if( (int) $uri[count($uri)-1]){
+          global $id;
+          $id = $uri[count($uri)-1];
+          array_pop($uri);
+      }
+      $new_uri = implode('/',$uri);
+
+      // /posts or /post
+
+      foreach ($this->routes as $route){
+          $web_uri = explode('/', $route['uri']);
+          if(preg_match('/{/',$web_uri[count($web_uri)-1])){
+            array_pop($web_uri);
+          }
+          $new_web_uri = implode('/',$web_uri); 
+          
+          if($new_uri=== $new_web_uri && $method===$route['method']){
+               $class = new $route['controller'];
+
+               call_user_func(array($class,$route['function']),$id);
+          }
+      }
 
    }
 
--- vendors/larashol/routing/router.php
+++ vendors/larashol/routing/router.php
@@ -1,11 +1,14 @@
 <?php
 
+
 use vendors\larashol\routing\RouterCore;
 
 $url = check_url();
 $router  =new RouterCore();
 $method = isset($_POST['_method']) ? $_POST['_method'] : $_SERVER['REQUEST_METHOD'];
-require ('routes/web.php');
+require 'routes/web.php';
+
+
 
 
 $router->resolve($url,$method);
